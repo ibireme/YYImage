04023b3a1938d90245c3f6b2978930f98a57367d
Pinlin <pinlin168@gmail.com>
2020-08-20 23:43:22 +0800

    fix: fix ios14 cannot display static webp

--- YYImage/YYAnimatedImageView.m
+++ YYImage/YYAnimatedImageView.m
@@ -528,6 +528,11 @@ typedef NS_ENUM(NSUInteger, YYAnimatedImageType) {
 - (void)displayLayer:(CALayer *)layer {
     if (_curFrame) {
         layer.contents = (__bridge id)_curFrame.CGImage;
+    } else {
+        // If we have no animation frames, call super implementation. iOS 14+ UIImageView use this delegate method for rendering.
+        if ([UIImageView instancesRespondToSelector:@selector(displayLayer:)]) {
+            [super displayLayer:layer];
+        }
     }
 }
 
